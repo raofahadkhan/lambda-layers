fed29b8a06b8ad03544343a3375e3aacc6d70c2b
Rao Fahad <raofahad046@gmail.com>
2023-11-11 14:50:34 +0500

    created separate dev and main environments

--- bin/lambda-layers.ts
+++ bin/lambda-layers.ts
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { LambdaLayersStack } from '../lib/lambda-layers-stack';
-
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { LambdaLayersStack } from "../lib/lambda-layers-stack";
+const service = "livecart-products-api";
+let stage;
 const app = new cdk.App();
-new LambdaLayersStack(app, 'LambdaLayersStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+stage = "m";
+new LambdaLayersStack(app, `${service}-${stage}`, {
+  tags: {
+    service,
+    stage,
+  },
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+stage = "d";
+new LambdaLayersStack(app, `${service}-${stage}`, {
+  tags: {
+    service,
+    stage,
+  },
 });
--- lib/lambda-layers-stack.ts
+++ lib/lambda-layers-stack.ts
@@ -1,10 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaLayersStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const { service, stage } = props?.tags!;
 
     // The code that defines your stack goes here
 
